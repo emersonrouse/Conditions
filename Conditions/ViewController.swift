6ca644bf15e9b804c811477932a7d91d2971e232
Emerson <emersonmixrouse@gmail.com>
2023-01-30 10:12:58 -0700

    Initial Commit

---
+++ Conditions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conditions
+//
+//  Created by Rouse, Emerson - Student on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
